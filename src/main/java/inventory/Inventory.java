d2c79050bbb7848fada2463ee5e7049c84d1b02d
ronggurmahendra <ronggur.mwp@gmail.com>
2021-04-25 00:31:47 +0700

    updating addItem Inventory

--- src/main/java/inventory/Inventory.java
+++ src/main/java/inventory/Inventory.java
@@ -57,7 +57,7 @@ public class Inventory<E extends Engimon,I extends Skill_Item> {
         }
 
         for(int i = 0; i< this.ListItem.size();i++){
-            if(this.ListItem.get(i).equals(Item)){
+            if(this.ListItem.get(i).getSkillName() == Item.getSkillName()){
                 this.ListItem.get(i).addAmount(Item.getAmount());
                 found = true;
             }
